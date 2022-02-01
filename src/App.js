f76534cbe3d5114f104f2af70e30d590893c67af
Laiza Benevides Sobral <71191403+laizabsobral@users.noreply.github.com>
2022-01-31 21:06:11 -0300

    adiciona music

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './styles/global.js';
 import Carrinho from './pages/Carrinho/Carrinho.js';
@@ -5,21 +6,30 @@ import Details from './pages/Details/Details.js';
 import Home from './pages/Home/Home.js';
 import Login from './pages/Login/Login.js';
 import Perfil from './pages/Perfil/Perfil.js';
+import {Provider} from "react-redux";
+import Store from "../src/redux/store";
+
 
 function App() {
   return (
+      
       <BrowserRouter basename="/">
+        <Provider store={Store}>
         <GlobalStyle />
         <div>
+         
           <Routes>
             <Route path='/' element={<Login />} />
             <Route path='/login' element={<Login />} />
             <Route path="/Home" element={<Home />} />
-            <Route path="/Perfil" element={<Perfil />} />
+  
+            {/* <Route path="/Perfil" element={<Perfil />} />
             <Route path="/Details" element={<Details />} />
-            <Route path="/Carrinho" element={<Carrinho />} />
+            <Route path="/Carrinho" element={<Carrinho />} /> */}
+            
           </Routes>
           </div>
+       </Provider>   
     </BrowserRouter>
   );
 }
--- src/services/Api.js
+++
@@ -1,7 +0,0 @@
-export default function Api() {
-
-    return (
-        <div></div>
-    );
-
-}
