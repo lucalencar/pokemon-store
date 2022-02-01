45763d9290d3efdc6ee2fb0484dd765f255afade
Dayane Garcia <daygds12@gmail.com>
2022-02-01 11:14:40 -0300

    atualizando package-lock

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,38 @@
 import { useNavigate } from "react-router-dom";
+import styled from 'styled-components'
+import * as S from "./Login.style"
+import Background from '../../img/background.jpg'
+import { logDOM } from "@testing-library/react";
+
+
 
 export default function Login() {
 
     const navigate = useNavigate();
 
     return (
-        <div>Login</div>
-    );
+        
+        <div>
+            <S.Navbar>Pokemon Store</S.Navbar>
+            <S.Login>
+                <h3>Login</h3>
+        <div>
+            <label for="Login">E-mail</label>
+            <input type="text" placeholder="Digite um email"/>
+           
+            <label for="Password">Senha</label>
+            <input type="text" placeholder="Digite um email"/>
+            <div>
+             <S.Button>Clique</S.Button>
+        </div>
+         </div>
+            </S.Login>
 
-}
+        
+            <S.Imagem src={Background}/>
+           <div>
+            <S.Footer>@by Pokemon</S.Footer>
+        </div>
+        </div>
+    );
+    }
---
+++ src/pages/Login/Login.style.js
@@ -0,0 +1,85 @@
+import styled from 'styled-components'
+
+
+export const Formulario = styled.form`
+    color: red;
+    border-radius: 50%;
+    position: absolute;
+    top: -50%;
+    left: 112px;  
+    width: 100%;
+    height: auto;  
+    
+`;
+
+export const Button = styled.button`
+   color: black;
+   
+   border-radius: 8px;
+`
+
+export const Container = styled.body`
+
+background-size: cover;
+opacity: 85%;
+
+`
+export const Login = styled.h1`
+border: 10px solid #000;
+font-size: 170%;
+width: 320px;
+height: 415px;
+border-radius:80px 0px 80px 0px;
+background-color: rgba(10,8,5,9,8);
+top:  40%;
+left: 30%;
+position: absolute;
+transform: translate(-555, -50%);
+box-sizing: border-box;
+padding: 35px 25px;
+color: #fff;
+background-color: black;
+opacity:78%;
+
+h3{
+   font-size: 95%;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+}
+`
+
+
+export const Nav = styled.nav`
+background-color: black;
+color: #fff;
+display: flex;
+padding: 1%;
+`
+
+export const Navbar = styled.header`
+ background-color: black; 
+ padding: 3%;
+ color: #fff;
+ display: flex;
+align-items: center;
+justify-content: center;
+
+`
+export const Imagem = styled.img`
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;    
+`
+
+export const Footer = styled.footer`
+ background-color: black;
+ padding: 3%;
+ align-items: center;
+ justify-content:center;
+ color: #fff;
+ margin: 5%;
+ height: 100%;
+ display: flex;
+ position: relative;
+`
