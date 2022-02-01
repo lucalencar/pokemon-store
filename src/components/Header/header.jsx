89fc078cded1d3e355644bdcd5d78ec188c3ec51
Laiza Benevides Sobral <71191403+laizabsobral@users.noreply.github.com>
2022-02-01 08:43:05 -0300

    Adicionando nav

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -2,9 +2,9 @@ import * as S from "./header.css";
 import React from 'react';
 import Logo from "../../../src/img/Logo.svg";
 import Music from "../Player/Music";
-
 import { AiOutlineUser, AiOutlineLogout, AiOutlineShoppingCart } from "react-icons/ai";
 import PokemonList from "../Pokemons/PokemonLista";
+//adicionando nav
 function Header() {
 
     return(
